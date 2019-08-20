552cf6218b42797b2cbfd38e90890cc5b0647730
Ricardo M <ricardomalosso@gmail.com>
2019-08-20 14:45:56 -0300

    frontend atualizado para popular a caixa de mensagens quando o usuário faz uma seleção

--- js/contact_me.js
+++ js/contact_me.js
@@ -35,7 +35,7 @@ $(function() {
           $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
           $('#success > .alert-success')
-            .append("<strong>Your message has been sent. </strong>");
+            .append("<strong>Sua mensagem foi enviada. </strong>");
           $('#success > .alert-success')
             .append('</div>');
           //clear all fields
@@ -46,7 +46,7 @@ $(function() {
           $('#success').html("<div class='alert alert-danger'>");
           $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+          $('#success > .alert-danger').append($("<strong>").text("Opa " + firstName + ", parece que ocorreu um erro. Tente novamente mais tarde!"));
           $('#success > .alert-danger').append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
